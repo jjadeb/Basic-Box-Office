4c1eb69157f8e1ae7d9ce65d4ef516c0441d175c
Jade Bouchard <jtakbouchard@gmail.com>
2022-01-27 10:13:18 -0800

    Done project

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import model.people.Patron;
